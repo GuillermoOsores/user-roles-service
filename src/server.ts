a90d5cd1ec9811e698e0d1ff5925b6ba3d2e0095
Guillermo Osores <guillermo.osores@us.pwc.com>
2018-12-06 16:07:17 -0300

    change to env port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
 import './models/dbconnection';
 
-app.listen(3000, function () {
-    console.log('listening on 3000');
+app.listen(process.env.PORT || 3000, function () {
+    console.log(`listening on ${process.env.PORT || 3000}`);
 });
