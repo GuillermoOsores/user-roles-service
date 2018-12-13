79f6b3558df16336d65b4519e2fceb5a201d8156
Guillermo Osores <guillermo.osores@us.pwc.com>
2018-12-13 15:25:42 -0300

    cosmo

--- src/models/dbconnection.ts
+++ src/models/dbconnection.ts
@@ -1,7 +1,8 @@
 import { connect, connection } from 'mongoose';
 //const mongoose = require( 'mongoose' );
 
-var dbURI = "mongodb://localhost/trainingApp";
+//var dbURI = "mongodb://localhost/trainingApp";
+var dbURI = "mongodb://ifspocnodemongo:pQ89JcJVLExJjDu8kciwAgMUm2hTRk5yLvniImJTTvf070xDLMlSGxn2S1aA29kFFTjT3FSlLHUo48Vv8GtUmw==@ifspocnodemongo.documents.azure.com:10255/trainingApp?ssl=true&replicaSet=globaldb";
 
 connect(dbURI, { useNewUrlParser: true });
 
@@ -16,3 +17,4 @@ connection.on('error', function (err) {
 connection.on('disconnected', function () {
     console.log('Mongoose disconnected');
 });
+
